6bd1f030c8c7f90961965f6df65d4e21077a2253
bbestbt <oranichbest@gmail.com>
2020-12-26 00:09:05 +0700

    add to cart attract

--- frontend/gotravel/src/Cart.js
+++ frontend/gotravel/src/Cart.js
@@ -10,44 +10,96 @@ import axios from 'axios';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Recipe from './Recipe'
+import cart from "./carts.png"
 
-
-import { removeItem,addQuantity,subtractQuantity} from './actions/cartActions'
+import { removeItem, addQuantity, subtractQuantity } from './actions/cartActions'
 
 
 
 
 class Cart extends Component {
 
-    handleRemove = (id)=>{
+    handleRemove = (id) => {
         this.props.removeItem(id);
     }
     //to add the quantity
-    handleAddQuantity = (id)=>{
+    handleAddQuantity = (id) => {
         this.props.addQuantity(id);
     }
     //to substruct from the quantity
-    handleSubtractQuantity = (id)=>{
+    handleSubtractQuantity = (id) => {
         this.props.subtractQuantity(id);
     }
 
-    render() { 
-        return(
+    render() {
+        let addedItems = this.props.items.length ?
+            (
+                this.props.items.map(item => {
+                    return (
+
+
+                        <li key={item.id}>
+                            <div >
+                                <img src={item.img} alt={item.img} />
+                            </div>
+
+                            <div >
+                                <span >{item.title}</span>
+                                <p><b>Price: {item.price}$</b></p>
+                                <p>
+                                    <b>Quantity: {item.quantity}</b>
+                                </p>
+
+                                <button onClick={() => { this.handleRemove(item.id) }}>Remove</button>
+                            </div>
+
+                        </li>
+
+                    )
+                })
+            ) :
+
+            (
+                <div>
+                <br></br>
+                <h5 align="center">Nothing</h5>
+                </div>
+            )
+        return (
+
             <div>
-                {/* <NavbarMain /> */}
-                    <div class="topBanner" >
-                    
-
-
-
-
-
-
+                <div class="topBanner" >
                 </div>
 
-            </div>
+                <div id="bgAttract">
+                    <br></br>
+                    <div  class="register-header" >
+                    <img src={cart} width="60" height="60" ></img>
+                        <h1 align="center">Order</h1>
+                        </div>
+                        <ul >
+                            {addedItems}
+                        </ul>
+                    
+                    </div>
+                    {/* <Recipe /> */}
+                </div>
         )
     }
 }
+// export default Cart;
 
-export default Cart;
+const mapStateToProps = (state) => {
+    return {
+                    items: state.addedItems,
+        //addedItems: state.addedItems
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+                    removeItem: (id) => { dispatch(removeItem(id))},
+        addQuantity: (id) => { dispatch(addQuantity(id))},
+        subtractQuantity: (id) => { dispatch(subtractQuantity(id))}
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
--- frontend/gotravel/src/attractTicket.jsx
+++ frontend/gotravel/src/attractTicket.jsx
@@ -7,30 +7,11 @@ import NavbarMain from "./components/Navbar";
 //     Switch,
 //   } from 'react-router-dom';
 import axios from 'axios';
-
+import { connect } from 'react-redux'
 import attract from "./promo/attract.png"
-import p1 from "./promo/P1.png"
-import p2 from "./promo/P2.png"
-import p3 from "./promo/P3.png"
-import p5 from "./promo/P5.png"
-import p6 from "./promo/P6.png"
-import p7 from "./promo/P7.png"
-import p8 from "./promo/P8.png"
-import p9 from "./promo/P9.png"
-import p10 from "./promo/p10.png"
-import p11 from "./promo/p11.png"
-import p12 from "./promo/p12.png"
-import p13 from "./promo/p13.png"
-import p14 from "./promo/p14.png"
-import p15 from "./promo/p15.png"
-import p16 from "./promo/p16.png"
-import p17 from "./promo/p17.png"
-import p18 from "./promo/p18.png"
-import p19 from "./promo/p19.png"
-import p20 from "./promo/p20.png"
 import {attractTicketURL,addToCartURL} from './constants'
 import { authAxios } from './utils';
-
+import { addToCart } from './actions/cartActions'
 class attractTicket extends Component {
 
     state={
@@ -52,19 +33,23 @@ class attractTicket extends Component {
         })
     }
 
-    handleAddToCart=slug=>{
-        this.setState({loading:true})
-        authAxios
-        // axios
-        .post(addToCartURL,{slug})
-        .then(res=>{
-            console.log(res.data)
-            //update cart count
-            this.setState({loading:false})
-        })
-        .catch(err=>{
-            this.setState({error:err,loading:false})
-        })
+    // handleAddToCart=slug=>{
+    //     this.setState({loading:true})
+    //     authAxios
+    //     // axios
+    //     .post(addToCartURL,{slug})
+    //     .then(res=>{
+    //         console.log(res.data)
+    //         //update cart count
+    //         this.setState({loading:false})
+    //     })
+    //     .catch(err=>{
+    //         this.setState({error:err,loading:false})
+    //     })
+    // }
+
+    handleClick = (id)=>{
+        this.props.addToCart(id); 
     }
 
     render() { 
@@ -91,7 +76,7 @@ class attractTicket extends Component {
             <h3 >{item.title}</h3>
             <div>
             <img  src={item.image} class="promoImg"  ></img>
-            <button align ="right" class="ticket" onClick={()=>this.handleAddToCart(item.slug)}>Add to cart </button>
+            <button align ="right" class="ticket"  onClick={()=>{this.handleClick(item.id)}}>Add to cart </button>
             </div>
             </div>
               })}
@@ -178,4 +163,17 @@ class attractTicket extends Component {
         );
     }
 }
-export default attractTicket;
+// export default attractTicket;
+const mapStateToProps = (state)=>{
+    return {
+      items: state.items
+    }
+  }
+const mapDispatchToProps= (dispatch)=>{
+    
+    return{
+        addToCart: (id)=>{dispatch(addToCart(id))}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(attractTicket)
