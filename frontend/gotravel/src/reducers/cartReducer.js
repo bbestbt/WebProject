06bcd4dd63f853a5862d9a5ea111e366bd8995cb
jui2542123 <45987444+jui2542123@users.noreply.github.com>
2020-12-27 02:15:45 +0700

    Add files via upload
    change to hotel

--- frontend/gotravel/src/reducers/cartReducer.js
+++ frontend/gotravel/src/reducers/cartReducer.js
@@ -1,21 +1,27 @@
-import Item1 from '../images/P1.png'
-import Item2 from '../images/P2.png'
-import Item3 from '../images/P3.png'
-import Item4 from '../images/P5.png'
-import Item5 from '../images/P6.png'
-import Item6 from '../images/P7.png'
-import Item7 from '../images/P8.png'
-import Item8 from '../images/P9.png'
-import Item9 from '../images/p10.png'
-import Item10 from '../images/p11.png'
-import Item11 from '../images/p12.png'
-import Item12 from '../images/p13.png'
-import Item13 from '../images/p14.png'
-import Item14 from '../images/p15.png'
-import Item15 from '../images/p16.png'
-import Item16 from '../images/p17.png'
-import Item17 from '../images/p18.png'
-import Item18 from '../images/p20.jpg'
+// import Item1 from '../images/P1.png'
+// import Item2 from '../images/P2.png'
+// import Item3 from '../images/P3.png'
+// import Item4 from '../images/P5.png'
+// import Item5 from '../images/P6.png'
+// import Item6 from '../images/P7.png'
+// import Item7 from '../images/P8.png'
+// import Item8 from '../images/P9.png'
+// import Item9 from '../images/p10.png'
+// import Item10 from '../images/p11.png'
+// import Item11 from '../images/p12.png'
+// import Item12 from '../images/p13.png'
+// import Item13 from '../images/p14.png'
+// import Item14 from '../images/p15.png'
+// import Item15 from '../images/p16.png'
+// import Item16 from '../images/p17.png'
+// import Item17 from '../images/p18.png'
+// import Item18 from '../images/p20.jpg'
+import Item1 from '../images/p35.jpg'
+import Item2 from '../images/p33.jpg'
+import Item3 from '../images/p32.jpg'
+import Item4 from '../images/p29.jpg'
+import Item5 from '../images/p34.jpg'
+import Item6 from '../images/p28.jpg'
 
 
 import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/cart-actions'
@@ -23,24 +29,30 @@ import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '
 
 const initState = {
     items: [
-        {id:1,title:'SAFARI WORLD', desc: ".", price:2550,img:Item1},
-        {id:2,title:'SAFARI WORLD', desc: ".", price:1950,img: Item2},
-        {id:3,title:'SAFARI WORLD', desc: ".",price:720,img: Item3},
-        {id:4,title:'VANA NAVA WATER JUNGLE', desc: ".", price:950,img: Item4},
-        {id:5,title:'VANA NAVA WATER JUNGLE', desc: ".",price:590,img: Item5},
-        {id:6,title:'VANA NAVA WATER JUNGLE', desc: ".", price:1350,img: Item6},
-        {id:7,title:'VANA NAVA WATER JUNGLE', desc: ".",price:2890,img: Item7},
-        {id:8,title:'VANA NAVA WATER JUNGLE', desc: ".", price:5200,img: Item8},
-        {id:9,title:'PATTAYA SHEEP FARM', desc: ".", price:70,img: Item9},
-        {id:10,title:'PATTAYA SHEEP FARM', desc: ".", price:200,img: Item10},
-        {id:11,title:'PATTAYA SHEEP FARM', desc: ".", price:140,img: Item11},
-        {id:12,title:'PATTAYA SHEEP FARM', desc: ".", price:550,img: Item12},
-        {id:13,title:'PATTAYA DOLPHINARIUM', desc: ".", price:200,img: Item13},
-        {id:14,title:'PATTAYA DOLPHINARIUM', desc: ".", price:270,img: Item14},
-        {id:15,title:'PATTAYA DOLPHINARIUM', desc: ".", price:270,img: Item15},
-        {id:16,title:'PATTAYA DOLPHINARIUM', desc: ".", price:335,img: Item16},
-        {id:17,title:'PATTAYA DOLPHINARIUM', desc: ".", price:335,img: Item17},
+        // {id:1,title:'SAFARI WORLD', desc: ".", price:2550,img:Item1},
+        // {id:2,title:'SAFARI WORLD', desc: ".", price:1950,img: Item2},
+        // {id:3,title:'SAFARI WORLD', desc: ".",price:720,img: Item3},
+        // {id:4,title:'VANA NAVA WATER JUNGLE', desc: ".", price:950,img: Item4},
+        // {id:5,title:'VANA NAVA WATER JUNGLE', desc: ".",price:590,img: Item5},
+        // {id:6,title:'VANA NAVA WATER JUNGLE', desc: ".", price:1350,img: Item6},
+        // {id:7,title:'VANA NAVA WATER JUNGLE', desc: ".",price:2890,img: Item7},
+        // {id:8,title:'VANA NAVA WATER JUNGLE', desc: ".", price:5200,img: Item8},
+        // {id:9,title:'PATTAYA SHEEP FARM', desc: ".", price:70,img: Item9},
+        // {id:10,title:'PATTAYA SHEEP FARM', desc: ".", price:200,img: Item10},
+        // {id:11,title:'PATTAYA SHEEP FARM', desc: ".", price:140,img: Item11},
+        // {id:12,title:'PATTAYA SHEEP FARM', desc: ".", price:550,img: Item12},
+        // {id:13,title:'PATTAYA DOLPHINARIUM', desc: ".", price:200,img: Item13},
+        // {id:14,title:'PATTAYA DOLPHINARIUM', desc: ".", price:270,img: Item14},
+        // {id:15,title:'PATTAYA DOLPHINARIUM', desc: ".", price:270,img: Item15},
+        // {id:16,title:'PATTAYA DOLPHINARIUM', desc: ".", price:335,img: Item16},
+        // {id:17,title:'PATTAYA DOLPHINARIUM', desc: ".", price:335,img: Item17},
         // {id:18,title:'',desc: ".",price:578, img: Item18},
+        {id:1,title:'El barrio lanna hotel',desc: "",price:1546.0,img:Item1},
+        {id:2,title:'Rainforest Chiangmai Hotel',desc: "",price:727.0,img:Item2},
+        {id:3,title:'Panan Krabi Resort',desc: "",price:1528.0,img:Item3},
+        {id:4,title:'D Varee Diva Central Rayong',desc: "",price:1155.0,img:Item4},
+        {id:5,title:'Natee The Riverfront Hotel',desc: "",price:1660.0,img:Item5},
+        {id:6,title:'BlueSotel Krabi',desc: "",price:1190.0,img:Item6},
     ],
     addedItems:[],
     total: 0
