67b08a4d93f649997a371387390e45553c4ec5d7
VarutAunvilai <55436783+VarutAunvilai@users.noreply.github.com>
2020-12-27 07:46:26 +0700

    แก้ชื่อไฟล์ข้างใน

--- frontend/gotravel/src/pages/Home.js
+++ frontend/gotravel/src/pages/Home.js
@@ -1,22 +1,17 @@
 import React, { Component } from "react";
 import "../App.css";
-import Hero from "../components/Hero";
+import Home1 from "../components/Home1";
 import About from "../components/About";
-// import Package from "../components/Package";
-// import Services from "../components/Services";
-import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-import NavbarMain from "../components/Navbar";
 import Province from '../province'
 class Home extends Component {
   render() {
     return (
       <div id='home'>
-        {/* <NavbarMain /> */}
-        <Hero />
+        <Home1 />
         <About />
         <Province/>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     );
   }
