5ce3fb09b7982f7e6150ed2f225d406fe58cd2bf
jui2542123 <45987444+jui2542123@users.noreply.github.com>
2020-12-27 11:27:45 +0700

    Add files via upload
    Fix attract + Food ticket

--- frontend/gotravel/src/attractTicket.jsx
+++ frontend/gotravel/src/attractTicket.jsx
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import NavbarMain from "./components/Navbar";
-// import {
-//     BrowserRouter as Router,
-//     Link,
-//     Route,
-//     Switch,
-//   } from 'react-router-dom';
+import {
+    Redirect,
+  } from 'react-router-dom';
 import axios from 'axios';
 import { connect } from 'react-redux'
 import attract from "./promo/attract.png"
@@ -17,7 +14,9 @@ class attractTicket extends Component {
     state={
         loading:false,
         error:null,
-        data:[]
+        data:[],
+        redirect:false,
+        id:null,
     }
 
     componentDidMount(){
@@ -33,34 +32,29 @@ class attractTicket extends Component {
         })
     }
 
-    // handleAddToCart=slug=>{
-    //     this.setState({loading:true})
-    //     authAxios
-    //     // axios
-    //     .post(addToCartURL,{slug})
-    //     .then(res=>{
-    //         console.log(res.data)
-    //         //update cart count
-    //         this.setState({loading:false})
-    //     })
-    //     .catch(err=>{
-    //         this.setState({error:err,loading:false})
-    //     })
-    // }
-
-    // handleClick = (id)=>{
-    //     this.props.addToCart(id); 
-    // }
-    handleClick() {
-        alert("full booking")
-      }
+    
+    click = (id)=>{
+        console.log(id)
+        this.props.addToCart(id);
+        console.log(id)
+        id = id -1
+        console.log(this.state.data[id])
+        this.setState({redirect: true});
+        this.setState({id: (id)});
+    }
 
     render() { 
         const {data,error,loading} =this.state;
         return (
         <div>
-            {/* {error && alert(JSON.stringify(error))} */}
-             {/* <NavbarMain /> */}
+              {this.state.redirect ? (
+                <Redirect
+                    to={{
+                    pathname: "/cart",
+                    state: { property_id: this.state.data[this.state.id] },
+                    }}
+                />
+                    ) : null} 
         <div class="topBanner" >
         </div>
         
@@ -79,106 +73,32 @@ class attractTicket extends Component {
             <h3 >{item.title}</h3>
             <div>
             <img  src={item.image} class="promoImg"  ></img>
-            <button align ="right" class="ticket"  onClick={this.handleClick} 
-            // onClick={()=>{this.handleClick(item.id)}}
-            >Add to cart </button>
+            <button align ="right" class="ticket"  onClick={this.handleClick}
+            onClick={() => {this.click(item.id)}}
+             >Add to cart </button>
             </div>
             </div>
               })}
         </div>
       
-        {/* <div id="placePromo">
-            <br></br>
-            <h3 align="center">SAFARI WORLD</h3>
-            <div align="center" >
-                <img src={p1} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket </a>
-                <img src={p2} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket </a>
-                <img src={p3} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-            </div>
-            
-        </div>
-    
-        <div id="placePromo">
-            <br></br>
-            <h3 align="center">VANA NAVA WATER JUNGLE</h3>
-            <div align="center">
-                <img src={p5} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p6} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p7} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p8} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p9} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-
-            </div>
-        </div>
-
-        <div id="placePromo">
-            <br></br>
-            <h3 align="center">PATTAYA SHEEP FARM</h3>
-            <div align="center">
-                <img src={p10} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p11} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p12} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p13} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-    
-
-            </div>
-        </div>
-
-        <div id="placePromo">
-            <br></br>
-            <h3 align="center">PATTAYA DOLPHINARIUM</h3>
-            <div align="center">
-                <img src={p14} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p15} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p16} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p17} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p18} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p19} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-                <img src={p20} class="promoImg"  ></img>
-                <a class="ticket" href="#"> Buy ticket  </a>
-
-            </div>
-        </div> */}
-
 
         </div>
         </div>
 
-   
-       
-        
         );
     }
 }
-export default attractTicket;
-// const mapStateToProps = (state)=>{
-//     return {
-//       items: state.items
-//     }
-//   }
-// const mapDispatchToProps= (dispatch)=>{
+// export default attractTicket;
+const mapStateToProps = (state)=>{
+    return {
+      items: state.items
+    }
+  }
+const mapDispatchToProps= (dispatch)=>{
     
-//     return{
-//         addToCart: (id)=>{dispatch(addToCart(id))}
-//     }
-// }
+    return{
+        addToCart: (id)=>{dispatch(addToCart(id))}
+    }
+}
 
-// export default connect(mapStateToProps,mapDispatchToProps)(attractTicket)
+export default connect(mapStateToProps,mapDispatchToProps)(attractTicket)
--- frontend/gotravel/src/foodTicket.jsx
+++ frontend/gotravel/src/foodTicket.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import NavbarMain from "./components/Navbar";
-import {
-    // BrowserRouter as Router,
-    Link,
-    // Route,
-    // Switch,
-} from 'react-router-dom';
 import food from "./promo/food.png"
 import {foodTicketURL,addToCartURL} from './constants'
 import { authAxios } from './utils';
 import axios from 'axios';
+import {
+    Redirect,
+  } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { addToCart } from './actions/cartActions'
 class foodTicket extends Component {
     state={
         loading:false,
         error:null,
-        data:[]
+        data:[],
+        redirect:false,
+        id:null,
     }
 
     componentDidMount(){
@@ -50,14 +49,28 @@ class foodTicket extends Component {
     // handleClick = (id)=>{
     //     this.props.addToCart(id); 
     // }
-    handleClick() {
-        alert("full booking")
-      }
+    click = (id)=>{
+        console.log(id)
+        this.props.addToCart(id);
+        console.log(id)
+        id = id -1
+        console.log(this.state.data[id])
+        this.setState({redirect: true});
+        this.setState({id: (id)});
+    }
 
     render() {
         const {data,error,loading} =this.state;
         return (
             <div>
+                        {this.state.redirect ? (
+                <Redirect
+                    to={{
+                    pathname: "/cart",
+                    state: { property_id: this.state.data[this.state.id] },
+                    }}
+                />
+                    ) : null} 
                 {/* <NavbarMain /> */}
                 <div class="topBanner" >
                 </div>
@@ -79,68 +92,34 @@ class foodTicket extends Component {
                                 <h6 class="caption">{food.promo}</h6>
                                 <h4 class="caption">{food.price} Baht</h4>
                                 <button align ="right" class="ticket"  onClick={this.handleClick}
-                                 onClick={()=>{this.handleClick(food.id)}}
+                                 onClick={() => {this.click(food.id)}}
                                  >Add to cart </button>
                             </div>
                             
-                            </div>
-                             })}
+                    </div>
+                        })}
                         
-                            
-
-                    {/* <div id="placePromo">
-                        <br></br>
-                        <div align="center" >
-                            <div class="allHotel">
-                                <Link to="/food1"> <img src="https://resource.nationtv.tv/photo_news/2018/09/21/1024_76h85hi66bfg7ae8dgd8a.jpg" class="promoImg"  ></img></Link>
-                                <h4 class="caption" >You&I Premium Suki Buffet</h4>
-                            </div>
-
-                            <div class="allHotel">
-                                <Link to="/food2"> <img src="https://img.wongnai.com/p/1920x0/2018/08/29/2c0fa01b619c417e9033590089ffffb8.jpg" class="promoImg"  ></img></Link>
-                                <h4 class="caption" >AKIYOSHI</h4>
-                            </div>
-
-                            <div class="allHotel">
-                                <Link to="/food3"> <img src="https://www.makalius.co.th/wp-content/post-images/186131-post/neta-grill-850-net-1429.jpg" class="promoImg"  ></img></Link>
-                                <h4 class="caption" >Neta Grill Buffet</h4>
-                            </div>
-
-                            <div class="allHotel">
-                                <Link to="/food4"> <img src="https://img.wongnai.com/p/1920x0/2020/05/25/8e4db61c237347b599cd9f55a2236ca0.jpg" class="promoImg"  ></img></Link>
-                                <h4 class="caption" >Kagonoya Premium Shabu</h4>
-                            </div>
-
-
-                            <div class="allHotel">
-                                <Link to="/food5"> <img src="https://cdn.eatigo.com/eatigo_MoMoCafeMarriottSathornVista_20180313120229_3198.jpg" class="promoImg"  ></img></Link>
-                                <h4 class="caption" >MoMo Cafe</h4>
-                            </div>
-
-
-                        </div>
-                    </div> */}
                 </div>
             </div>
 
-</div>
+        </div>
 
 
         );
         
     }
 }
-export default foodTicket;
-// const mapStateToProps = (state)=>{
-//     return {
-//       items: state.items
-//     }
-//   }
-// const mapDispatchToProps= (dispatch)=>{
+// export default foodTicket;
+const mapStateToProps = (state)=>{
+    return {
+      items: state.items
+    }
+  }
+const mapDispatchToProps= (dispatch)=>{
     
-//     return{
-//         addToCart: (id)=>{dispatch(addToCart(id))}
-//     }
-// }
+    return{
+        addToCart: (id)=>{dispatch(addToCart(id))}
+    }
+}
 
-// export default connect(mapStateToProps,mapDispatchToProps)(foodTicket)
+export default connect(mapStateToProps,mapDispatchToProps)(foodTicket)
