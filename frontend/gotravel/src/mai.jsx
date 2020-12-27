5582a8e86ef038541d4640c64586112b8178cdad
VarutAunvilai <55436783+VarutAunvilai@users.noreply.github.com>
2020-12-27 08:14:50 +0700

    แก้รูปหาย

--- frontend/gotravel/src/mai.jsx
+++ frontend/gotravel/src/mai.jsx
@@ -24,7 +24,7 @@ class mai extends Component {
         </div>
 
         <div class="placeProvince">
-        <Link to="/mai1"> <img src={require("./north/doi inthanon.jpg")} alt="doi inthanon" class="allPlace" ></img></Link>
+        <Link to="/mai1"> <img src="https://d3h30waly5w5yx.cloudfront.net/images/tour/pictures/doi-inthanon-national-park-1-day-tour-tripguru-1.jpg" class="allPlace" ></img></Link>
             <br></br>
             <br></br>
             <h6 align="center">Doi Inthanon</h6>          
@@ -32,7 +32,7 @@ class mai extends Component {
 
 
         <div class="placeProvince">
-        <Link to="/mai2"> <img src={require("./north/doi pui.jpg")} alt="doi pui" class="allPlace"  ></img></Link>
+        <Link to="/mai2"> <img src="https://static.asiawebdirect.com/m/bangkok/portals/chiangmai-bangkok-com/homepage/attractions/doi-pui/allParagraphs/BucketComponent/ListingContainer/01/image/doi-pui-village.jpg" class="allPlace" ></img></Link>
             <br></br>
             <br></br>
             <h6 align="center">Doi Pui</h6>
@@ -41,7 +41,7 @@ class mai extends Component {
 
 
         <div class="placeProvince">
-        <Link to="/mai3"> <img src={require("./north/doi suthep.jpg")} alt="doi suthep"  class="allPlace"  ></img></Link>
+        <Link to="/mai3"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Phra_That_Doi_Suthep_01.jpg/1200px-Phra_That_Doi_Suthep_01.jpg" class="allPlace" ></img></Link>
             <br></br>
             <br></br>
             <h6 align="center">Wat Phra That Doi Suthep</h6>
@@ -50,7 +50,7 @@ class mai extends Component {
 
 
         <div class="placeProvince">
-        <Link to="/mai4"> <img src={require("./north/wat chedi luang.jpg")} alt="wat chedi luang"  class="allPlace"   ></img></Link>
+        <Link to="/mai4"> <img src="https://blog.airpaz.com/wp-content/uploads/Wat-Chedi-Luang.jpg" class="allPlace" ></img></Link>
             <br></br>
             <br></br>
             <h6 align="center">Wat Chedi Luang</h6>
